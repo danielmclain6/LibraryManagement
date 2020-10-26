aa2ee09ccb3f800acd8039fd8b1b6fd05cec3d99
liamcclane <lia_mcclane@msn.com>
2020-10-26 12:49:10 -0700

    change you connection manager approitatly

--- src/main/java/com/cognixia/jump/library/connection/ConnectionManager.java
+++ src/main/java/com/cognixia/jump/library/connection/ConnectionManager.java
@@ -1,49 +1,43 @@
 package com.cognixia.jump.library.connection;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionManager {
+	// where is the path/connection we are going to
+	private static final String URLWindows = "jdbc:mysql://localhost:3306/library";
+	private static final String URLMAC = "";
+	private static final String USERNAME = "root";
+	private static final String PASSWORDWindows = "root"; // Windows : root, Mac : Root@123
+	private static final String PASSWORDMac = ""; // Windows : root, Mac : Root@123
+	
+
+	// singleton example
 	private static Connection connect; // will be null atm
 
 	private static void makeConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Registered Driver");
+			System.out.println("NEWWWW");
 
-			Properties props = new Properties();
-			props.load(new FileInputStream("resources/config.properties"));
-
-			String url = props.getProperty("url");
-			String username = props.getProperty("username");
-			String password = props.getProperty("password");
-
-			connect = DriverManager.getConnection(url, username, password);
+			connect = DriverManager.getConnection(URLWindows, USERNAME, PASSWORDWindows);
 			System.out.println("Connected");
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
 	/**
 	 * this just checks if the singleton object has been connected
-	 * 
 	 * @return Connection object
 	 */
 	public static Connection getConnection() {
-		if (connect == null) {
+		if(connect == null) {
 			makeConnection();
 		}
 		return connect;
