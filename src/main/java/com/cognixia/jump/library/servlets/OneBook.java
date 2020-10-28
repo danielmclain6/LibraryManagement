671c74193fe40cd8e2f16d21584502bde54fdb5e
liamcclane <lia_mcclane@msn.com>
2020-10-28 11:02:53 -0700

    RESTART TOMCAT WHEN MAKING NEW SERVELTSgit add .

--- src/main/java/com/cognixia/jump/library/servlets/OneBook.java
+++ src/main/java/com/cognixia/jump/library/servlets/OneBook.java
@@ -9,17 +9,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cognixia.jump.library.connection.ConnectionManager;
 import com.cognixia.jump.library.dao.BookDao;
 import com.cognixia.jump.library.dao.BookDaoImp;
+import com.cognixia.jump.library.models.Book;
 
 /**
- * Servlet implementation class OneBook
+ * Servlet implementation class LogoutServlet
  */
-@WebServlet("/one_book")
+@WebServlet("/book")
 public class OneBook extends HttpServlet {
-	private static final long serialVersionUID = 13464673L;
+	private static final long serialVersionUID = 1L;
+
 	private BookDao bookDao;
 
 	@Override
@@ -39,23 +42,27 @@ public class OneBook extends HttpServlet {
 		}
 
 	}
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("------in the onebookServelt.java file");
+		HttpSession session = request.getSession();
+		Book book = bookDao.getBookByIsbn(request.getParameter("isbn"));
 		
-		System.out.println(request.getParameter("isbn"));
+		// models onto jsps
+		request.setAttribute("book", book);
+		request.setAttribute("user", 
+				session.getAttribute("user") == null ? null : session.getAttribute("user"));
+		request.setAttribute("isLibrarian", 
+				session.getAttribute("isLibrarian") == null ? null : session.getAttribute("isLibrarian"));
 		
-		
+		// render template
 		RequestDispatcher dispatcher = request.getRequestDispatcher("book.jsp");
-		System.out.println("sent");
 		dispatcher.forward(request, response);
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
---
+++ src/main/java/com/cognixia/jump/library/servlets/SingleBook.java
@@ -0,0 +1,38 @@
+package com.cognixia.jump.library.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SingleBook
+ */
+@WebServlet("/single_book")
+public class SingleBook extends HttpServlet {
+	private static final long serialVersionUID = 14565L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("hello world");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("book.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
